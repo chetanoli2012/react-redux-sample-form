66310515cdc25540b52565621e045acc788330e4
chetan <chetan.oli@squareyards.co.in>
2020-09-28 03:26:36 +0530

    Add formik integration to inputParams form

--- src/components/input-params/inputParams.jsx
+++ src/components/input-params/inputParams.jsx
@@ -1,38 +1,77 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Tooltip } from '@material-ui/core'
 import { InfoRounded } from '@material-ui/icons'
+import { Formik } from 'formik'
 
 function InputParams() {
+
+
+  const dispatchToTheStore = (values) => {
+    console.log('Here we go!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!', values);
+  }
+
   return (
     <Card className="card">
       <CardHeader title="Input Params" className="card--header"></CardHeader>
       <CardContent className="card--body">
-        <form className="form--input-params" noValidate autoComplete="off">
-          <FormControl fullWidth={true} margin="dense" className="form--wrapper">
-            <TextField label="Index of Label Column" className="field--input" />
-            <Tooltip title="Index of Label Column" aria-label="info" className="tool--tip">
-              <InfoRounded></InfoRounded>
-            </Tooltip>
-          </FormControl>
-          <FormControl fullWidth={true} margin="dense">
-            <FormLabel className="text--left">Automatic Cat. Column Flag</FormLabel>
-            <div className = 'mini--wrapper'>
-              <RadioGroup row aria-label="auto-cat-column" name="autoCatColumn" className="field--input">
-                <FormControlLabel value="yes" control={<Radio />} label="Yes" />
-                <FormControlLabel value="no" control={<Radio />} label="No" />
-              </RadioGroup>
-              <Tooltip title="Info" aria-label="info">
-                <InfoRounded></InfoRounded>
-              </Tooltip>
-            </div>
-          </FormControl>
-          <FormControl fullWidth={true} margin="dense" className="form--wrapper">
-            <TextField label="Categorical Columns" className="field--input" />
-            <Tooltip title="Info" aria-label="info">
-              <InfoRounded></InfoRounded>
-            </Tooltip>
-          </FormControl>
-        </form>
+        <Formik
+          initialValues={{
+            inputParams: {
+              labelColumn: undefined,
+              autoCatColumn: undefined,
+              categoricalColumns: undefined
+            }
+          }}
+        >
+          {
+            ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue }) =>
+              (
+                <>
+                  {console.log(values)}
+                  <FormControl fullWidth={true} margin="dense" className="form--wrapper">
+                    <TextField label="Index of Label Column" className="field--input" name="inputParams.labelColumn"
+                      value={values.inputParams.labelColumn} onChange={handleChange}
+                      onBlur={() => {
+                        dispatchToTheStore(values);
+                      }}
+                    />
+                    <Tooltip title="Index of Label Column" aria-label="info" className="tool--tip">
+                      <InfoRounded></InfoRounded>
+                    </Tooltip>
+                  </FormControl>
+                  <FormControl fullWidth={true} margin="dense">
+                    <FormLabel className="text--left">Automatic Cat. Column Flag</FormLabel>
+                    <div className='mini--wrapper'>
+                      <RadioGroup row aria-label="auto-cat-column" className="field--input"
+                        name="inputParams.autoCatColumn" onChange={handleChange}
+                        onBlur={() => {
+                          dispatchToTheStore(values);
+                        }}
+                      >
+                        <FormControlLabel value="yes" control={<Radio />} label="Yes" />
+                        <FormControlLabel value="no" control={<Radio />} label="No" />
+                      </RadioGroup>
+                      <Tooltip title="Automatic Cat. Column Flag" aria-label="info">
+                        <InfoRounded></InfoRounded>
+                      </Tooltip>
+                    </div>
+                  </FormControl>
+                  <FormControl fullWidth={true} margin="dense" className="form--wrapper">
+                    <TextField label="Categorical Columns" className="field--input"
+                      name="inputParams.categoricalColumns" onChange={handleChange}
+                      onBlur={() => {
+                        dispatchToTheStore(values);
+                      }}
+                      value={values.inputParams.categoricalColumns}
+                    />
+                    <Tooltip title="Categorical Columns" aria-label="info">
+                      <InfoRounded></InfoRounded>
+                    </Tooltip>
+                  </FormControl>
+                </>
+              )
+          }
+        </Formik>
       </CardContent>
     </Card>
   )
