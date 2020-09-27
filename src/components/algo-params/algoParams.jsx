a88038d49d7e88b0ea30ddb0c122dedc6c2c53e3
chetan <chetan.oli@squareyards.co.in>
2020-09-28 04:29:34 +0530

    Configure state and dispatch for the store to algoParms and set the value to be picked up from store if present

--- src/components/algo-params/algoParams.jsx
+++ src/components/algo-params/algoParams.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Card, CardHeader, CardContent, FormControl, FormControlLabel, FormLabel, Input, InputLabel, MenuItem, Radio, RadioGroup, Select, Tooltip } from '@material-ui/core'
 import { InfoRounded } from '@material-ui/icons';
 import { Formik } from 'formik'
+import { connect } from "react-redux";
+import * as form from "../../store/ducks/formData.duck";
 
-function AlgoParams() {
+function AlgoParams(props) {
 
   const dispatchToTheStore = (values) => {
-    console.log('Here we go!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!',values);
+    props.loadAlgoParamsData(values);
   }
 
   return (
@@ -16,13 +18,13 @@ function AlgoParams() {
         <Formik
           initialValues={{
             algoParams: {
-              hyperParamTuning: undefined,
-              kFoldFlag: undefined,
-              trainTestRandomSplitType: undefined,
-              trainingDataPercentage: undefined,
-              testDataPercentage: undefined,
-              maxDepthOfTree: undefined,
-              maxNumberOfBins: undefined
+              hyperParamTuning: props?.algoParams?.hyperParamTuning || undefined,
+              kFoldFlag: props?.algoParams?.kFoldFlag || undefined,
+              trainTestRandomSplitType: props?.algoParams?.trainTestRandomSplitType || undefined,
+              trainingDataPercentage: props?.algoParams?.trainingDataPercentage || undefined,
+              testDataPercentage: props?.algoParams?.testDataPercentage || undefined,
+              maxDepthOfTree: props?.algoParams?.maxDepthOfTree || undefined,
+              maxNumberOfBins: props?.algoParams?.maxNumberOfBins || undefined
             }
           }}
         >
@@ -30,11 +32,11 @@ function AlgoParams() {
             ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, setFieldValue }) =>
               (
                 <>
-                  {console.log('values for algoparams', values)}
                   <FormControl fullWidth={true} margin="dense">
                     <FormLabel className="text--left">Hyperparameter tuning</FormLabel>
                     <div className='mini--wrapper'>
                       <RadioGroup row aria-label="hyperparameter-tuning" className="field--input"
+                        value={values.algoParams.hyperParamTuning}
                         name="algoParams.hyperParamTuning" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -52,6 +54,7 @@ function AlgoParams() {
                     <FormLabel className="text--left">K Fold Flag</FormLabel>
                     <div className='mini--wrapper'>
                       <RadioGroup row aria-label="k-fold-flag" className="field--input"
+                        value={values.algoParams.kFoldFlag}
                         name="algoParams.kFoldFlag" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -68,6 +71,7 @@ function AlgoParams() {
                   <FormControl fullWidth={true} margin="dense" className="form--wrapper">
                     <InputLabel id="train-test-split-type" className="text--left">Train Test Random Split Type</InputLabel>
                     <Select labelId="train-test-split-type" className="text--left field--input"
+                      value={values.algoParams.trainTestRandomSplitType}
                       name="algoParams.trainTestRandomSplitType" onChange={handleChange}
                       onBlur={() => {
                         dispatchToTheStore(values);
@@ -86,6 +90,7 @@ function AlgoParams() {
                     <FormLabel className="text--left">Training Data Percentage</FormLabel>
                     <div className='mini--wrapper'>
                       <Input type="number" inputProps={{ step: "0.1" }} className="field--input"
+                        value={values.algoParams.trainingDataPercentage}
                         name="algoParams.trainingDataPercentage" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -100,6 +105,7 @@ function AlgoParams() {
                     <FormLabel className="text--left">Test Data Percentage</FormLabel>
                     <div className='mini--wrapper'>
                       <Input type="number" inputProps={{ step: "0.1" }} className="field--input"
+                        value={values.algoParams.testDataPercentage}
                         name="algoParams.testDataPercentage" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -114,6 +120,7 @@ function AlgoParams() {
                     <FormLabel className="text--left">Maximum Depth of Tree</FormLabel>
                     <div className='mini--wrapper'>
                       <Input type="number" className="field--input"
+                        value={values.algoParams.maxDepthOfTree}
                         name="algoParams.maxDepthOfTree" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -127,6 +134,7 @@ function AlgoParams() {
                     <FormLabel className="text--left">Maximum Number of Bins</FormLabel>
                     <div className="mini--wrapper">
                       <Input type="number" className="field--input"
+                        value={values.algoParams.maxNumberOfBins}
                         name="algoParams.maxNumberOfBins" onChange={handleChange}
                         onBlur={() => {
                           dispatchToTheStore(values);
@@ -146,4 +154,11 @@ function AlgoParams() {
   )
 }
 
-export default AlgoParams
+const mapStateToProps = state => ({
+  algoParams: state.formData && state.formData.algoParams
+});
+
+export default connect(
+  mapStateToProps,
+  form.actions
+)(AlgoParams)
